1a79242b144b6dbe04c40cfd3b678b906da9fdb4
Brianna <briannakincart@gmail.com>
2018-04-26 09:47:24 -0400

    One more addition

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,8 +1,6 @@
 class Author < ApplicationRecord
   has_many :books
 
-  # If we will want access to the full name in multiple places, we want to define it here.
-  # If we only need it in one serializer or controller, we could define it on that file.
   def full_name
     "#{first_name} #{last_name}"
   end
