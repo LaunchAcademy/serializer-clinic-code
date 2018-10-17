dcafca8d54a37131b18fe798ac46e4174e212db8
Brianna Kincart <briannakincart@gmail.com>
2018-10-17 10:17:30 -0400

    Change to FactoryBot

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -1,5 +1,5 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :author do
-    
+
   end
 end
--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,5 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :review do
-    
+
   end
 end
